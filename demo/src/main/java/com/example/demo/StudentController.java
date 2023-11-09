d659736ba27e61603916caa67d384c37d4a48718
maya elabed <56444556+mayaelabed@users.noreply.github.com>
2023-11-09 11:29:10 +0100

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    // build rest API to handle query parameters
+    // http://localhost:8080/student/query?firstName=Ramesh&lastName=Fadatare
+    @GetMapping("/student/query")
+    public Student studentQueryParam(
+            @RequestParam(name = "firstName") String firstName,
+            @RequestParam(name = "lastName") String lastName) {
+        return new Student(firstName, lastName);
+    }
+}
